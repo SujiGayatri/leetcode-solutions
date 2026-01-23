a1a0baaa05c328eb530083a241c6ad6f3a5dc5c1
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-01-23 20:49:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.1 MB (18.08%) - LeetSync

---
+++ 979-di-string-match/di-string-match.py
@@ -0,0 +1,14 @@
+class Solution:
+    def diStringMatch(self, s: str) -> List[int]:
+        n = len(s)
+        low, high = 0, n
+        ans = []
+        for ch in s:
+            if ch == 'I':
+                ans.append(low)
+                low += 1
+            else:
+                ans.append(high)
+                high -= 1
+        ans.append(low)  
+        return ans 
