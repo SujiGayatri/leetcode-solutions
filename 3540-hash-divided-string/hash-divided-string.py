d96086f6c37d54dc510fad6ac33810d729658902
SujiGayatri <119868379+SujiGayatri@users.noreply.github.com>
2026-04-04 22:42:42 +0530

    Time: 19 ms (40.00%) | Memory: 19.2 MB (94.92%) - LeetSync

---
+++ 3540-hash-divided-string/hash-divided-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def stringHash(self, s: str, k: int) -> str:
+        res = ""
+        for i in range(0, len(s), k):
+            substring = s[i:i+k]
+            total = sum(ord(c) - ord('a') for c in substring)
+            res += chr(total % 26 + ord('a'))
+        return res
